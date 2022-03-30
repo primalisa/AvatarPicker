1d55cc02ad529edd9a66cef048e37123a3d6011b
prima <primalisha112k@gmail.com>
2022-03-30 08:50:25 +0530

    Initial Commit

---
+++ src/AvatarPicker/MainAvatar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Proptypes from "prop-types";
+import "./avatar-picker-styles/main-avatar.css";
+
+MainAvatar.propTypes = {
+  handleClick: Proptypes.func.isRequired,
+  currentAvatar: Proptypes.object.isRequired
+};
+
+function MainAvatar({ handleClick, currentAvatar }) {
+ 
+  return (
+    <button className="Avatar" onClick={handleClick}>
+ 
+      <img
+        src={require(`../avatars/${currentAvatar.src}`)}
+        className="Avatar-img"
+        alt={`MainAvatar: ${currentAvatar.label}`}
+      />
+    </button>
+  );
+}
+
+export default MainAvatar;
